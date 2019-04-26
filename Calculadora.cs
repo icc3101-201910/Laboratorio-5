3a0f387f7aead1a5a78a52cb44178f8d876e2d6b
Nicolás Gómez <nicolas@mtblc.co>
2019-04-26 18:16:12 -0400

    :tada: incremento 1

---
+++ Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace laboratorio5
+{
+    public class Calculadora
+    {
+        Random random ;
+        private int numero;
+        public List<ICalcular> calculadoras;
+
+        public Calculadora(int numero)
+        {
+            this.numero = numero;
+            calculadoras = new List<ICalcular>();
+            random = new Random(); // para que no se repita el mismo número https://stackoverflow.com/q/767999/2056593
+        }
+
+        public int GetNumero()
+        {
+            return numero;
+        }
+
+        public void Calcular()
+        {
+            var calculadoraAlAzar = calculadoras[random.Next(calculadoras.Count)];
+            numero = calculadoraAlAzar.Calcular(numero);
+        }
+    }
+}
---
+++ ICalcular.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace laboratorio5
+{
+    public interface ICalcular
+    {
+        int Calcular(int numero);
+    }
+}
